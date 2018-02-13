48ea748c986a18d76452db862a032a89846ebaec
ian-ramos <ramos.ianc@gmail.com>
2018-02-13 16:53:08 -0500

    started adding about me text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,15 +5,16 @@ class AboutMe extends React.Component {
 
   render() {
     return (
-      <Grid>
+      <Grid id='AboutMe'>
         <Grid.Row centered columns={3}>
           <Grid.Column>
-            <Segment>
-              Hello
+            <Segment raised id="about-me-text">
+              <p>Hi! My name is Ian Ramos.  I graduated from Fordham University in 2012 with a BS in Business Administration (concentration in Finance) and a minor in Economics.  The following year I got my MBA in Finance, also from Fordham.</p>
+              <p>After 5 years as a Financial Analyst, I decided to pursue my passion in programming and enroll in The Flatiron School.  This is a 15 week boot camp which teaches, Ruby, Ruby on Rails, Javascript, React, Redux, and many more frameworks.</p>
             </Segment>
           </Grid.Column>
           <Grid.Column>
-            <Segment>
+            <Segment raised>
               World
             </Segment>
           </Grid.Column>
