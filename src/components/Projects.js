4c9ad887d9e3115e0882231f97b0cdaa52e41dca
ian-ramos <ramos.ianc@gmail.com>
2018-02-27 12:08:40 -0500

    added grid layout for projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { Grid, Segment } from 'semantic-ui-react'
 
 class Projects extends React.Component {
 
   render() {
     return (
-      <div id='Projects'>
-        Hi
-      </div>
+      <Grid id='Projects'>
+        <Grid.Row centered columns={2}>
+          <Grid.Column>
+            <Segment raised>
+              
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row centered columns={2}>
+          <Grid.Column>
+            <Segment raised>
+
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row centered columns={2}>
+          <Grid.Column>
+            <Segment raised>
+
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
