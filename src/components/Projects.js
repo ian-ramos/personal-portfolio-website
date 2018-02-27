c1224ec2878f0935b85577ae2014650a2154a0c1
ian-ramos <ramos.ianc@gmail.com>
2018-02-27 12:22:53 -0500

    fixed styling, added links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ class Projects extends React.Component {
         <Grid.Row centered columns={2}>
           <Grid.Column>
             <Segment raised>
-              <img src={require("../images/nerd-alert-screenshot.png")} alt="Nerd Alert" id="nerd-alert-screenshot"/>
+              <a href="https://nerd-alert-frontend.herokuapp.com/"><img src={require("../images/nerd-alert-screenshot.png")} alt="Nerd Alert" id="nerd-alert-screenshot"/></a>
             </Segment>
           </Grid.Column>
         </Grid.Row>
@@ -17,7 +17,7 @@ class Projects extends React.Component {
         <Grid.Row centered columns={2}>
           <Grid.Column>
             <Segment raised>
-              <img src={require("../images/set-game-screenshot.png")} alt="Set Game" id="set-game-screenshot"/>
+              <a href="https://set-game-frontend.herokuapp.com/"><img src={require("../images/set-game-screenshot.png")} alt="Set Game" id="set-game-screenshot"/></a>
             </Segment>
           </Grid.Column>
         </Grid.Row>
@@ -25,7 +25,7 @@ class Projects extends React.Component {
         <Grid.Row centered columns={2}>
           <Grid.Column>
             <Segment raised>
-              <img src={require("../images/bass-o-matic-screenshot.png")} alt="Bass-O-Matic" id="bass-o-matic-screenshot"/>
+              <a href="https://ian-ramos.github.io/mod-3-final-project-frontend/"><img src={require("../images/bass-o-matic-screenshot.png")} alt="Bass-O-Matic" id="bass-o-matic-screenshot"/></a>
             </Segment>
           </Grid.Column>
         </Grid.Row>
